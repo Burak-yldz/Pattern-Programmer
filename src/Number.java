3ebc7e16e0f89e7c6e9a63f1c0f5743cca555d0f
Burak-yldz <burak_yldz@outlook.com.tr>
2022-06-07 17:13:04 +0200

    pattern

---
+++ src/Number.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Number {
+
+    static int negative(int x) {
+        if (x <= 0){
+            return x;
+        }
+        else {
+            System.out.print(x + " ");
+            return negative(x-5);
+        }
+    }
+
+    static int positive(int x,int y) {
+        if(x > y){
+            return x;
+        }
+        else {
+            System.out.print(x + " ");
+            return positive(x+5,y);
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        while (true){
+            System.out.println("Please enter a number : ");
+            int number = input.nextInt();
+
+            positive(negative(number),number);
+
+            if (number == 0){
+                break;
+            }
+        }
+    }
+}
